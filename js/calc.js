644f8119e5ef0d08ed8f3ffc36615e1456aad2af
shevchenkoak <demorest49de@gmail.com>
2022-04-20 17:58:06 +0300

    c

--- js/calc.js
+++ js/calc.js
@@ -2,6 +2,7 @@ const add = document.getElementById('add')
 const subtract = document.getElementById('subtract')
 const multiply = document.getElementById('multiply')
 const divide = document.getElementById('divide')
+const clearAll = document.getElementById('clearAll')
 
 const x = document.getElementById('x')
 const y = document.getElementById('y')
@@ -12,6 +13,7 @@ add.addEventListener('click', doAdd)
 subtract.addEventListener('click', doSubtract)
 multiply.addEventListener('click', doMultiply)
 divide.addEventListener('click', doDivide)
+clearAll.addEventListener('click', doclearAll)
 
 add.addEventListener('mouseover', checkNumbers)
 subtract.addEventListener('mouseover', checkNumbers)
@@ -19,7 +21,12 @@ multiply.addEventListener('mouseover', checkNumbers)
 divide.addEventListener('mouseover', checkNumbers)
 
 
-
+function doclearAll() {
+    x.value = '0'
+    y.value = '0'
+    result.value = '0'
+    saveCalcValues()
+}
 
 function checkNumbers() {
     if ((x.value.trim()) == '' || y.value.trim() == '') {
@@ -27,6 +34,7 @@ function checkNumbers() {
     }
 
 }
+
 function doAdd() {
     const expression = +(x.value) + +(y.value)
     result.value = expression
@@ -34,15 +42,21 @@ function doAdd() {
 }
 
 function doSubtract() {
-
+    const expression = +(x.value) - +(y.value)
+    result.value = expression
+    saveCalcValues()
 }
 
 function doMultiply() {
-
+    const expression = +(x.value) * +(y.value)
+    result.value = expression
+    saveCalcValues()
 }
 
 function doDivide() {
-
+    const expression = +(x.value) / +(y.value)
+    result.value = expression
+    saveCalcValues()
 }
 
 function createCalc() {
@@ -65,12 +79,12 @@ function getCalcValues() {
         const js = JSON.parse(localStorage.getItem('calc'))
         console.log(js)
         return JSON.parse(localStorage.getItem('calc'))
-    }else {
+    } else {
         return createCalc()
     }
 }
 
-function renderValues(){
+function renderValues() {
     x.value = calc.x
     y.value = calc.y
     result.value = calc.result
